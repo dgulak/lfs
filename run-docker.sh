a135c34105b4e1b2a95efcb928b7c4b3163b9d4b
Denis Gulak <d.gulak@ukr.net>
2018-07-20 23:48:38 +0200

    Added script for running docker

---
+++ run-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! ${LFS_DIR} ] ; then
+    echo -e "\e[31m LFS_DIR is not set\e[0m"
+    exit 1
+fi
+
+docker run -it --privileged \
+--mount "type=bind,src=${LFS_DIR}/docker/lfs.mount,dst=/mnt/lfs" \
+--mount "type=bind,src=${LFS_DIR}/scripts,dst=/work/scripts" \
+--mount "type=volume,src=lfs-work-dir,dst=/work" \
+dgulak/lfs:latest zsh
+
